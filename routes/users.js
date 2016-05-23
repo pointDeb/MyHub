d1dc1d8b9245be65cbec468603008f5ee0071f23
stephano romain <stephano.romain@gmail.com>
2016-05-24 00:57:57 +0300

    custumize basic layout

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.render('users.jade',{var:'test'});
+	res.render('users.jade',{title:'Users',arbre:'Users'});
 });
 router.get('/add',function(req,res,next){
-	res.render('users-add.jade',{title:'ajout membre'});
+	res.render('users-add.jade',{title:'ajout membre',arbre:'Users>add'});
 });
 
 module.exports = router;
