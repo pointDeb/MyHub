0f8d2f36f7f59d84b7aeaaa769b0c2952f939468
stephano romain <stephano.romain@gmail.com>
2016-05-24 12:05:01 +0300

    add data directory

---
+++ data/event.js
@@ -0,0 +1,17 @@
+var event={
+	posts: [
+			{
+				user:{pic:'/images/cover.jpg',name:'Un gas bien fute sur les bmx'},
+				content:"You're not connected And thee.•See if your wireless switch is turned on.•Restart your router "
+			},
+			{
+				user:{pic:'/images/cover.jpg',name:'Un gas bien fute sur les bmx'},
+				content:"You're not connected And thee.•See if your wireless switch is turned on.•Restart your router "
+			},
+			{
+				user:{pic:'/images/cover.jpg',name:'Un gas bien fute sur les bmx'},
+				content:"You're not connected And thee.•See if your wireless switch is turned on.•Restart your router "
+			}
+		  ]
+}
+module.exports = event;
---
+++ data/files.js
@@ -0,0 +1,13 @@
+var files={
+	sliders: [
+		{src:'/images/cover.jpg',title:'Bmx',desc:'Un gas bien fute sur les bmx'},
+		{src:'/images/lemurien.jpg',title:'Les lemuriens',desc:'Les lemiriens vivent tres bien dans la foret'},
+		{src:'/images/0.jpg',title:'Bmx',desc:'Un gas bien fute sur les bmx'},
+		{src:'/images/1.png',title:'Bmx',desc:'Un gas bien fute sur les bmx'},
+		{src:'/images/2.png',title:'Bmx',desc:'Un gas bien fute sur les bmx'},
+		{src:'/images/3.jpg',title:'Bmx',desc:'Un gas bien fute sur les bmx'},
+		{src:'/images/4.jpg',title:'Bmx',desc:'Un gas bien fute sur les bmx'},
+		{src:'/images/5.jpeg',title:'Bmx',desc:'Un gas bien fute sur les bmx'}
+	]
+}
+module.exports = files;
