ab1024e480f0542482a0dd15b235698fc5d44893
stephano romain <stephano.romain@gmail.com>
2016-05-24 10:58:24 +0300

    add materialize componnent

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var sliders= [
+	{src:'/images/cover.jpg',title:'Bmx',desc:'Un gas bien fute sur les bmx'},
+	{src:'/images/cover.jpg',title:'Bmx',desc:'Un gas bien fute sur les bmx'},
+	{src:'/images/cover.jpg',title:'Bmx',desc:'Un gas bien fute sur les bmx'}
+];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',sliders});
 });
 
 module.exports = router;
